0faa291d97a2bdd70b13e4f26bfe5aa6e3fa8dbd
Mahamood-Sameer <d.mahamoodsameer786@gmail.com>
2023-01-11 11:56:53 +0530

    Removed Async from useffect and wrote the logic on top

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -4,25 +4,25 @@ import { GetExperiences, GetNextExperiences } from "../api/experiences";
 
 const Experiences = () => {
   const [InterviewExperiences, setInterviewExperiences] = useState();
-  useEffect(async () => {
-    const results = await GetExperiences();
-    setInterviewExperiences(results);
+  useEffect(() => {
+    GetExperiences().then((results) => {
+      setInterviewExperiences(results);
+    });
   }, []);
+  const NextTenExperiences = async () => {
+    const results = await GetNextExperiences(InterviewExperiences?.next);
+    setInterviewExperiences(results);
+  };
   return (
     <div>
       {InterviewExperiences?.results?.map((interview, key) => {
-        return (
-          <>
-            <h1 key={key}>{interview.id}</h1>
-          </>
-        );
+        return <h1 key={key}>{interview.id}</h1>;
       })}
       {/* For Next 10 Experiences. */}
       {InterviewExperiences ? (
         <button
-          onClick={async () => {
-            const results = await GetNextExperiences(InterviewExperiences?.next);
-            setInterviewExperiences(results);
+          onClick={() => {
+            NextTenExperiences();
           }}
         >
           Next
