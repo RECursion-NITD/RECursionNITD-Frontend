bdf215c70a6665c718c52f9cf6a800013c4f9933
Mahamood-Sameer <d.mahamoodsameer786@gmail.com>
2023-01-11 09:33:10 +0530

    Added Interview Experiences

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+/* eslint-disable */
+import React, { useEffect, useState } from "react";
+import { GetExperiences, GetNextExperiences } from "../api/experiences";
 
 const Experiences = () => {
-  return <div>Interview Experiences</div>;
+  const [InterviewExperiences, setInterviewExperiences] = useState();
+  useEffect(async () => {
+    const results = await GetExperiences();
+    setInterviewExperiences(results);
+  }, []);
+  return (
+    <div>
+      {InterviewExperiences?.results?.map((interview, key) => {
+        return (
+          <>
+            <h1 key={key}>{interview.id}</h1>
+          </>
+        );
+      })}
+      {/* For Next 10 Experiences. */}
+      {InterviewExperiences ? (
+        <button
+          onClick={async () => {
+            const results = await GetNextExperiences(InterviewExperiences?.next);
+            setInterviewExperiences(results);
+          }}
+        >
+          Next
+        </button>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 };
 
 export default Experiences;
