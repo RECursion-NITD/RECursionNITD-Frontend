a51cbcc16e7727dd576dafe40b4ea2ca28bd3cc1
Ashesh Saha <78008554+hash-slash@users.noreply.github.com>
2023-01-12 22:39:35 +0530

    Update Events.jsx

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import React,{ useEffect, useState } from "react";
+import { getEvents,getNextEvents } from "../api/events";
 
 const Events = () => {
   return <div>Events</div>;
-};
+  const [Events, setEvents] = useState([]);
+  useEffect = () => {
+    (async () => {
+      try {
+        const response = await getEvents();
+      } catch(error) {
+        console.log(error);
+      }
+      setEvents(response);
+    },[])};
+    return (
+      <div>
+        {Events?.response?.map((event, key) => {
+          return (
+            <>
+              <h1 key={key}>{event.id}</h1>
+            </>
+          );
+        })}
+        {Events ? (
+          <button
+            onClick={async () => {
+              const response = await getNextEvents(Events?.next);
+              setEvents(response);
+            }}
+            >
+              Next 
+              </button>
+        ) : (
+          <></>
+        )} 
+      </div>
+    );
+  };
 
 export default Events;
