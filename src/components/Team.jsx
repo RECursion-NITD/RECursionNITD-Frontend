7d0af41731ae3b23bf83e38720f6f4ba1d973024
sujal3011 <jaiswalsujal639@gmail.com>
2023-09-17 11:24:27 +0530

    teams page changes

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,36 +1,442 @@
-import { useEffect, useState } from "react";
-import { getTeam } from "../api/team";
+import { useEffect, useState, useRef } from "react";
+import { getTeam, getAlumni } from "../api/team";
 import useLoading from "../hooks/useLoading";
 import Loader from "./Loader";
+import jsonData from "../dummydata/dummy_team.json";
+import {
+  Box,
+  Center,
+  Heading,
+  Image,
+  Link,
+  SimpleGrid,
+  Icon,
+  List,
+  ListItem,
+  Text,
+  Grid,
+  Card,
+  CardBody,
+  Stack,
+  Flex,
+} from "@chakra-ui/react";
+import { FaFacebook, FaLinkedin, FaPhoneSquare } from "react-icons/fa";
+import { ArrowLeftIcon, ArrowRightIcon } from "@chakra-ui/icons";
+
+const TeamMember = ({ member }) => {
+  {
+    console.log(member);
+  }
+  return (
+    <Box className="our-team" p="20%">
+      <Box className="pic" boxShadow="2px 2px 50px #0a0a0a">
+        <Image
+          src={member.image_url}
+          alt={member.name}
+          borderRadius="100%"
+          borderColor="#343A3F"
+          borderWidth="10px"
+          borderStyle="solid"
+        />
+
+        <List className="social-links">
+          <ListItem>
+            <Link href={member.url_Facebook} target="_blank" color="white">
+              <Icon as={FaFacebook} />
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link href={member.url_LinkedIn} target="_blank" color="white">
+              <Icon as={FaLinkedin} />
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link href={`tel:${member.mobile}`} color="white">
+              <Icon as={FaPhoneSquare} />
+            </Link>
+          </ListItem>
+        </List>
+      </Box>
+
+      <Heading className="team-title">{member.name}</Heading>
+      <Box color="#a7b3b3">{member.designation}</Box>
+    </Box>
+  );
+};
+
+// const MemberCard = ({ member }) => (
+//   <Box as="div" p={3} textAlign="center">
+//     <Image
+//       src={member.image_url}
+//       alt={member.name}
+//       borderRadius="full"
+//       border="5px solid #343A3F"
+//       boxSize="120px"
+//       objectFit="cover"
+//       mx="auto"
+//       mb={2}
+//       mt={3}
+//       boxShadow="2px 2px 50px #0a0a0a"
+//     />
+//     <Text color="#19e5d4" fontWeight="bold">
+//       {member.name}
+//     </Text>
+//     <Text fontSize="sm" color="gray.500">
+//       {member.designation}
+//     </Text>
+//   </Box>
+// );
+
+// const AlumniCard = ({ alumni }) => (
+//   <Box mb={2}>
+//     <Box
+//       className="jumbotron shadow-sm p-2 mb-3 bg-white rounded"
+//       p={2}
+//       borderRadius="md"
+//       boxShadow="sm"
+//       bg="white"
+//     >
+//       <Box display="flex">
+//         <Box flex="0 0 25%">
+//           <Image
+//             className="alumni rounded-circle shadow-sm"
+//             src={alumni.image_url}
+//             alt={alumni.name}
+//             borderRadius="full"
+//             border="4px solid #7F7F7F"
+//             boxSize="80px"
+//             objectFit="cover"
+//           />
+//         </Box>
+//         <Box flex="0 0 75%" pl={2}>
+//           <Text color="green.500" fontSize="sm">
+//             {alumni.name}
+//           </Text>
+//           <Text fontSize="sm">B.Tech. in {alumni.branch}</Text>
+//           <Link
+//             href={alumni.url_LinkedIn}
+//             target="_blank"
+//             rel="noopener noreferrer"
+//             color="blue.500"
+//             fontSize="sm"
+//             display="flex"
+//             alignItems="center"
+//           >
+//             <FaLinkedin style={{ marginRight: "0.5rem" }} /> LinkedIn Profile
+//           </Link>
+//         </Box>
+//       </Box>
+//     </Box>
+//   </Box>
+// );
+
+const AlumniCard2 = ({ alumni }) => (
+  <Card
+    direction={{ base: "column", sm: "row" }}
+    overflow="hidden"
+    variant="outline"
+    bg="#100f0f"
+    border="none"
+    boxShadow="2px 2px 50px #100e0e"
+  >
+    <Image
+      objectFit="cover"
+      // maxW={{ base: "100%", sm: "200px" }}
+      src={alumni.image}
+      alt={alumni.name}
+      width="40%"
+    />
+
+    <Stack width="60%">
+      <CardBody>
+        <Heading color="#a9f103" size="md">
+          {alumni.name}
+        </Heading>
+
+        <Text color="white" py="2">
+          B.Tech. in {alumni.branch}
+        </Text>
+        <Link
+          href={alumni.url_LinkedIn}
+          target="_blank"
+          rel="noopener noreferrer"
+          color="blue.500"
+          fontSize="sm"
+          display="flex"
+          alignItems="center"
+        >
+          <FaLinkedin style={{ marginRight: "0.5rem" }} /> LinkedIn Profile
+        </Link>
+      </CardBody>
+    </Stack>
+  </Card>
+);
 
 const Team = () => {
   const { loading, setLoading } = useLoading();
+  const scrollContainerRef = useRef(null);
 
-  const [team, setTeam] = useState();
+  const scroll = (direction) => {
+    const scrollDistance = 530;
+    if (scrollContainerRef.current) {
+      const scrollLeft = scrollContainerRef.current.scrollLeft;
+      if (direction === "left") {
+        scrollContainerRef.current.scrollTo({
+          left: scrollLeft - scrollDistance,
+          behavior: "smooth",
+        });
+      } else if (direction === "right") {
+        scrollContainerRef.current.scrollTo({
+          left: scrollLeft + scrollDistance,
+          behavior: "smooth",
+        });
+      }
+    }
+  };
+
+  const [alumni, setAlumni] = useState(null);
+  const [alumniYear, setAlumniYear] = useState(2023);
+  // const [selectedAlumniYear, setSelectedAlumniYear] = useState(null);
+
+  const [team, setTeam] = useState(null);
+
+  // const setBatch = () => {
+  //   console.log("Inside set batch function");
+  //   const years = [];
+  //   const currentDate = new Date();
+  //   const year = currentDate.getFullYear();
+  //   const month = currentDate.getMonth() + 1;
+
+  //   let currBatchYear;
+
+  //   if (month >= 6 && month <= 12) {
+  //     currBatchYear = year + 1;
+  //   } else {
+  //     currBatchYear = year;
+  //   }
+
+  //   years.push(currBatchYear);
+  //   years.push(currBatchYear + 1);
+
+  //   if (currBatchYear === year) {
+  //     years.push(currBatchYear + 2);
+  //   }
+  //   console.log(years);
+  //   setBatchYears(years);
+  //   console.log(batchYears);
+  // };
+
   useEffect(() => {
     setLoading(true);
+    getAlumni(alumniYear)
+      .then((data) => {
+        setAlumni(data);
+        setLoading(false);
+      })
+      .catch((err) => console.err(err));
+  }, [alumniYear]);
+
+  useEffect(() => {
+    setLoading(true);
+    // setBatch();
     getTeam()
-      .then((teamData) => {
-        setTeam(teamData);
+      .then((data) => {
+        console.log(data);
+        setTeam(data);
+
         setLoading(false);
       })
       .catch((err) => console.err(err));
   }, []);
 
+  useEffect(() => {
+    if (team) {
+      console.log(team["2024"]);
+    }
+  }, [team]);
+
   return (
-    <div>
+    <Box bg="#3e3b3b">
       {loading && <Loader />}
+
+      {/* TEAM HEADER */}
+      <Box p={4}>
+        <Center mt="5rem">
+          <Heading className="team-heading" mt={1} mb={3}>
+            Meet The Team
+          </Heading>
+        </Center>
+
+        {team &&
+          Object.keys(team).map((year) => {
+            <Box key={year}>
+              <Center mt="3rem">
+                <Heading className="team-heading" mt={1} mb={3} fontSize="2xl">
+                  Batch of {year}
+                </Heading>
+              </Center>
+
+              <SimpleGrid columns={[1, 2, 3, 4]} spacing={4}>
+                {team[year]?.length > 0 &&
+                  team[year].map((member, id) => {
+                    <TeamMember key={id} member={member} />;
+                  })}
+              </SimpleGrid>
+            </Box>;
+          })}
+
+        <SimpleGrid columns={[1, 2, 3, 4]} spacing={4}>
+          {team &&
+            team["2024"]?.length > 0 &&
+            team["2024"].map((member, id) => {
+              // console.log("hi") && <TeamMember key={id} member={member} />;
+              console.log("hi") && <Box key={id}>{member.batch_year}</Box>;
+            })}
+        </SimpleGrid>
+      </Box>
+
+      {/* ALUMNI SECTION */}
+
+      <Box mt={4}>
+        <Center>
+          <Heading
+            as="h1"
+            mt="5%"
+            mb="2%"
+            color="rgb(207, 188, 188)"
+            fontSize={{ base: "xl", md: "2xl" }}
+          >
+            Meet our Alumni
+          </Heading>
+        </Center>
+
+        <Flex width="90%" borderRadius="2rem" backgroundColor="grey">
+          <Box
+            w="40px"
+            h="40px"
+            borderRadius="50%"
+            bg="gray.300"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            cursor="pointer"
+            marginLeft="50px"
+            marginRight="20px"
+            onClick={() => scroll("left")}
+            _hover={{
+              transform: "scale(1.2)",
+              transition: "transform 0.3s ease-in-out",
+            }}
+          >
+            <ArrowLeftIcon color="black" />
+          </Box>
+
+          <Box
+            flex="60%"
+            mr={2}
+            overflowX="scroll"
+            whiteSpace="nowrap"
+            p={4}
+            ref={scrollContainerRef}
+            sx={{
+              "&::-webkit-scrollbar": {
+                display: "none",
+              },
+            }}
+          >
+            <Flex>
+              {jsonData.year_set.map((year) => (
+                <Box
+                  flex="0 0 33.33%"
+                  p={2}
+                  textAlign="center"
+                  key={year}
+                  _hover={{
+                    transform: "scale(1.2)",
+                    transition: "transform 0.3s ease-in-out",
+                  }}
+                >
+                  <Text
+                    color="white"
+                    borderBottom="1px solid white"
+                    display="inline"
+                    cursor="pointer"
+                    fontSize="lg"
+                    fontWeight="bold"
+                    onClick={() => {
+                      // setSelectedAlumniYear(year);
+                      setAlumni(null);
+                      setAlumniYear(year);
+                    }}
+                  >
+                    Batch of {year}
+                  </Text>
+                </Box>
+              ))}
+            </Flex>
+          </Box>
+
+          <Box
+            w="40px" // Set the width and height to create a circular shape
+            h="40px"
+            borderRadius="50%"
+            bg="gray.300"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            cursor="pointer"
+            marginLeft="20px"
+            marginRight="50px"
+            onClick={() => scroll("right")}
+            _hover={{
+              transform: "scale(1.2)",
+              transition: "transform 0.3s ease-in-out",
+            }}
+          >
+            <ArrowRightIcon color="black" />
+          </Box>
+        </Flex>
+
+        <Grid key={alumniYear} mb={2} direction="column">
+          <Heading
+            as="h1"
+            mt="1%"
+            mb="2%"
+            color="rgb(207, 188, 188)"
+            fontSize={{ base: "lg", md: "xl" }}
+            textAlign="center"
+          >
+            <small>Batch of {alumniYear}</small>
+          </Heading>
+
+          <Grid
+            px="3rem"
+            templateColumns={{
+              base: "repeat(1, 1fr)",
+              lg: "repeat(3, 1fr)",
+            }}
+            gap={4}
+          >
+            {alumni &&
+              alumni?.map((alumni, id) => (
+                <AlumniCard2 key={id} alumni={alumni} />
+              ))}
+          </Grid>
+        </Grid>
+      </Box>
 
       <h1>Team Page</h1>
 
-      {team &&
+      {/* {team &&
         team?.map((member, id) => (
           <div key={id}>
             <p>{member.name}</p>
             <p>{member.role}</p>
           </div>
-        ))}
-    </div>
+        ))} */}
+    </Box>
   );
 };
 
