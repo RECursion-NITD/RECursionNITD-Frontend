010201de72c62951e158bbaee9757910934f91c0
Sivam2313 <sivamdas7@gmail.com>
2023-09-19 10:05:41 +0530

    search

--- src/components/InterviewExperiences/SearchExperiences.jsx
+++ src/components/InterviewExperiences/SearchExperiences.jsx
@@ -1,27 +1,32 @@
 /* eslint-disable */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useLoading from "../../hooks/useLoading";
 import { SearchExp } from "../../api/experiences";
 
 const SearchExperiences = ({setInterviewExperiences}) => {
   const { loading, setLoading } = useLoading();
-  // const [companyName, setCompanyName] = useState('Company');
-    const queryHandler = async(e)=>{
+  const [companyName, setCompanyName] = useState("Company");
+    useEffect(() => {
       setLoading(true);
-      SearchExp(e.target.value).then((results)=>{
-          setInterviewExperiences(results);
-          setLoading(false);
-      })
-    }
+      if(companyName != "Company"){
+        SearchExp(companyName).then((results)=>{
+            setInterviewExperiences(results);
+            setLoading(false);
+        })
+      }else{
+        setLoading(false);
+      }
+    }, [companyName])
+    
     const companies = ["Company","Microsoft", "Adobe", "Wells Fargo", "BNY", "Texas Instruments", "Oracle","JP Morgan"];
     const rounds = ["Round","1","2","3"];
   return (
     <div className="w-full mt-4 mb-4 flex justify-start">
-        <select onChange={queryHandler} className="h-9 w-1/6 me-2 border-outline rounded-lg ps-3 pe-2">
+        <select onChange={(e)=>setCompanyName(e.target.value)} value={companyName} className="h-9 w-1/6 me-2 border-outline rounded-lg ps-3 pe-2">
             {companies.map((company,idx)=>{
-              return(
-                <option key={idx} value={company}>{company}</option>
-              );
+                return(
+                  <option key={idx} value={company}>{company}</option>
+                );
             })}
         </select>
         <select className="h-9 w-1/6 rounded-lg ps-3 pe-2">
