7ef0ecb32f5c91f1a3e361cf09fc3a0f1c2555c9
moinak878 <moinak878@gmail.com>
2023-09-30 11:52:34 +0530

    contents page ui

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -11,13 +11,18 @@ import { Link } from "react-router-dom";
 import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 import React, { useEffect, useState } from "react";
 import { getContents } from "../../api/getStarted";
+import useLoading from "../../hooks/useLoading";
+import Loader from "../Loader";
 
 const GetStarted = () => {
   const [contents, setContents] = useState(null);
+  const { loading, setLoading } = useLoading();
 
   useEffect(() => {
+    setLoading(true);
     getContents().then((result) => {
       setContents(result);
+      setLoading(false);
     });
   }, []);
 
@@ -36,46 +41,70 @@ const GetStarted = () => {
     <Flex
       minHeight={"92vh"}
       margin={0}
-      padding={0}
+      padding={"15px"}
       mt={"8vh"}
       width={"100%"}
-      justifyContent={"center"}
+      textColor={"white"}
       flexDirection={"column"}
-      alignItems={"center"}
       background={"gray.800"}
     >
-      <Container textAlign="center" marginTop={"5vh"}>
+      <Container
+        textAlign="center"
+        marginTop={"5vh"}
+        minWidth={"90vw"}
+        padding={"15px"}
+        background={"whiteAlpha.50"}
+        borderRadius={"8px"}
+      >
         <Heading> Getting Started</Heading>
-        <Text>The best way to learn is to get started</Text>
+        <p
+          className="text-gray-300 font-sub italic"
+          style={{
+            fontWeight: "bold",
+            marginBottom: "1em",
+          }}
+        >
+          The best way to learn is to get started
+        </p>
+        <Text>
+          Whether you{"'"}re a beginner or an experienced coder, this collection
+          provides in-depth insights into algorithms and data structures crucial
+          for competitive programming. We{"'"}re dedicated to expanding our
+          knowledge base by enhancing existing articles and adding new ones, all
+          aimed at empowering programmers in their coding journey.
+        </Text>
       </Container>
 
       <Container
-        border={"solid 1px gray"}
         borderRadius={"8px"}
         background={"gray.700"}
         padding={"15px"}
         minWidth={"90vw"}
         margin={"15px"}
+        textAlign="left"
       >
-        <Heading size="lg">Contents</Heading>
-        {!contents && <Text>Loading...</Text>}
+        {loading && <Loader />}
         {contents?.length > 0 && (
-          <Flex flexDirection={"column"}>
+          <Container flexDirection={"column"} minWidth={"80vw"}>
             {contents.map((level, levelIndex) => (
               <Box key={levelIndex} marginY="10px">
                 <Text
+                  borderRadius={"8px"}
+                  background={"gray.600"}
                   color={`#${level.Color}`}
                   fontSize="xl"
                   fontWeight="bold"
                   marginBottom="5px"
                 >
-                  {level.Level_title}
+                  LEVEL {levelIndex} : {level.Level_title}
                 </Text>
                 {level?.topic?.map((topic, topicIndex) => (
                   <Box key={topicIndex} marginY="5px">
                     <Flex alignItems="center">
                       <IconButton
                         size="sm"
+                        background={"gray.900"}
+                        _hover={{ background: "gray.800" }}
                         icon={
                           openTopics.includes(
                             `level${levelIndex}_topic${topicIndex}`
@@ -94,7 +123,9 @@ const GetStarted = () => {
                             : "Expand"
                         }
                       />
-                      <Text fontSize="md">{topic.Topic_title}</Text>
+                      <Text fontSize="md" padding={"15px"} fontWeight="bold">
+                        {topic.Topic_title}
+                      </Text>
                     </Flex>
                     <Collapse
                       in={openTopics.includes(
@@ -102,11 +133,17 @@ const GetStarted = () => {
                       )}
                     >
                       {topic?.subtopic?.map((subtopic, subtopicIndex) => (
-                        <Text fontSize="md" key={subtopicIndex}>
+                        <Text
+                          fontSize="md"
+                          key={subtopicIndex}
+                          width={"90%"}
+                          marginLeft={"4em"}
+                          padding={"5px"}
+                          borderRadius={"8px"}
+                          _hover={{ background: "gray.600" }}
+                        >
                           <Link to={`/get_started/${subtopic.id}`}>
-                            <Text fontWeight={"bold"} marginLeft={"3em"}>
-                              {subtopic.sub_topic}
-                            </Text>
+                            <Text>• {subtopic.sub_topic}</Text>
                           </Link>
                         </Text>
                       ))}
@@ -115,7 +152,7 @@ const GetStarted = () => {
                 ))}
               </Box>
             ))}
-          </Flex>
+          </Container>
         )}
       </Container>
     </Flex>
