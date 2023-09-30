eff421a5bd47b330fde88055b8253096adabc89a
sujal3011 <jaiswalsujal639@gmail.com>
2023-10-01 02:19:18 +0530

    Fixed the IE page's company and role filters

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -4,13 +4,18 @@ import React, { useEffect, useState } from "react";
 import { GetExperiences, GetNextExperiences } from "../api/experiences";
 import useLoading from "../hooks/useLoading";
 import Loader from "./Loader";
-import { Container } from '@chakra-ui/react';
+import { Container } from "@chakra-ui/react";
 import Entries from "./InterviewExperiences/Entries";
 import SearchExperiences from "./InterviewExperiences/SearchExperiences";
+import { SearchExp } from "../api/experiences";
 
 const Experiences = () => {
   const [InterviewExperiences, setInterviewExperiences] = useState();
   const { loading, setLoading } = useLoading();
+
+  const [search, setSearch] = useState("");
+  const [Company, setCompany] = useState(null);
+  const [InterviewType, setInterviewType] = useState(null);
 
   useEffect(() => {
     setLoading(true);
@@ -19,6 +24,24 @@ const Experiences = () => {
       setLoading(false);
     });
   }, []);
+
+  const FilterHandler = () => {
+    // console.log("Inside filterHandler", Company, InterviewType, search);
+    setLoading(true);
+    SearchExp(Company, InterviewType, search)
+      .then((response) => {
+        console.log(response);
+        setInterviewExperiences(response);
+        setLoading(false);
+        // setSearch("");
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    // console.log("Useeffect called now");
+    FilterHandler();
+  }, [Company, InterviewType]);
 
   const NextExperiences = async (url) => {
     const results = await GetNextExperiences(url);
@@ -32,12 +55,25 @@ const Experiences = () => {
   };
 
   const getDate = (created_at) => {
-    const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
     const date = new Date(created_at);
     const year = date.getFullYear();
     const month = monthNames[date.getMonth()];
     const day = date.getDate();
-    const dateString = day + ' ' + month + ', ' + year;
+    const dateString = day + " " + month + ", " + year;
     console.log(dateString);
     return dateString;
   };
@@ -48,18 +84,38 @@ const Experiences = () => {
     </div>
   ) : (
     <div>
-        <div>
-          <h1 className="text-onSurface font-head text-3xl mb-2">Interview Experiences</h1>
-          <p className="text-onSurface font-sub italic"> Nothing ever becomes real'til it is experienced - John Keats </p>
-        </div>
-        <div>
-          <SearchExperiences setInterviewExperiences={setInterviewExperiences}/>
-        </div>
-        <Container minW='container.sm' p={0} pt='4px'  borderBottom = '0px' maxW='container.xm' w='45vw' minH='container.sm' h='50vh'>
+      <div>
+        <h1 className="text-onSurface font-head text-3xl mb-2">
+          Interview Experiences
+        </h1>
+        <p className="text-onSurface font-sub italic">
+          {" "}
+          Nothing ever becomes real'til it is experienced - John Keats{" "}
+        </p>
+      </div>
+      <div>
+        <SearchExperiences
+          setInterviewExperiences={setInterviewExperiences}
+          setSearch={setSearch}
+          setCompany={setCompany}
+          setInterviewType={setInterviewType}
+          FilterHandler={FilterHandler}
+          Company={Company}
+          InterviewType={InterviewType}
+        />
+      </div>
+      <Container
+        minW="container.sm"
+        p={0}
+        pt="4px"
+        borderBottom="0px"
+        maxW="container.xm"
+        w="45vw"
+        minH="container.sm"
+        h="50vh"
+      >
         {InterviewExperiences?.results?.map((interview, key) => {
-          return (
-            Entries({key,interview})
-          );
+          return Entries({ key, interview });
         })}
 
         {/* For Previous 10 Experiences. */}
