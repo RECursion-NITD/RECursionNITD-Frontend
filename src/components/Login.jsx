fd959654f2ca397045add5fe161ac276a3acf30e
sathvika <chamasathvika04@gmail.com>
2024-12-18 23:16:05 +0530

    Add signup page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,27 @@
+// export const login = async (formData) => {
+//   const response = await fetch(
+//     "http://127.0.0.1:8000/api/token/", // TODO: use axi
+//     {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+
+//       body: JSON.stringify({
+//         username: formData.username,
+//         password: formData.password,
+//       }),
+//     }
+//   );
+//   const data = await response.json();
+//   console.log("data", data);
+//   return data;
+// };
+
+// export default login;
 /* eslint-disable */
+
+
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import React, { useState, useEffect } from "react";
@@ -121,3 +144,4 @@ const Login = () => {
 };
 
 export default Login;
+
