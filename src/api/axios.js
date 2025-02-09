5ccc5971e1e2f88a9dc4fbc064671cfa20776d6d
sathvika <chamasathvika04@gmail.com>
2025-02-09 23:43:18 +0530

    corrected prettier errors

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const api = process.env.REACT_APP_API || "https://recursion70.pythonanywhere.com/";
+const api =
+  process.env.REACT_APP_API || "https://recursion70.pythonanywhere.com/";
 
 export default axios.create({
   baseURL: api,
